499ddc140acc097ebafd1f968d220daa4ffe50c8
Jack Price <32805567+Ferexx@users.noreply.github.com>
2018-03-10 21:04:20 +0000

    Addition of comments to main

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+/* A program that implements the Reversi game in C,
+    written by Jack Price and Monika Pociute. */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,29 +14,28 @@ typedef struct disc {
     int yPos;
 } disc;
 
-int main()
-{
-    char theBoard[7][7]; int remainingDiscs=60;
+int main() {
+    char theBoard[7][7]; int remainingDiscs=60;     //Initialization of the board + initial positions of discs.
     theBoard[3][3]='W'; theBoard[4][4]='W';
     theBoard[3][4]='B'; theBoard[4][3]='B';
     player player1, player2;
-    printf("Player 1, please enter your name: ");
+    printf("Player 1, please enter your name: ");   //Getting player names and assignment of disc colors.
     scanf("%[^\n]s", player1.playerName);
     printf("Player 2, please enter your name: ");
     scanf("%[\n]s", player2.playerName);
     player1.discType='B';
     player2.discType='W';
-    printf("\n0 1 2 3 4 5 6 7 8\n");
+    printf("\n0 A B C D E F G H\n");                //Displaying board in console.
     for(int i=0;i<8;i++) {
         printf("%d", i+1);
         for(int j=0;j<8;j++) {
-            if(j==7&&(theBoard[i][j]=='B'||theBoard[i][j]=='W')) {
+            if(j==7&&(theBoard[i][j]=='B'||theBoard[i][j]=='W')) {      //Special cases if printing has reached end of line.
                 printf("|%c|\n", theBoard[i][j]);
                 break;
             }
             else if(j==7)
                 printf("|x|\n");
-            else if(theBoard[i][j]=='B'||theBoard[i][j]=='W'){
+            else if(theBoard[i][j]=='B'||theBoard[i][j]=='W'){          //Printing board.
                 printf("|%c", theBoard[i][j]);
             }
             else {
